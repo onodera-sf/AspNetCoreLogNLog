04df2ac5549af25ce0580c1f1fed20ebb4a28cdf
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-03-30 13:16:51 +0900

    プロジェクト ファイルを追加します。

---
+++ LogNLog/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LogNLog.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+
+			_logger.LogTrace("Trace で出力します。");
+			_logger.LogDebug("Debug で出力します。");
+			_logger.LogInformation("Information で出力します。");
+			_logger.LogWarning("Warning で出力します。");
+			_logger.LogError("Error で出力します。");
+			_logger.LogCritical("Critical で出力します。");
+
+			_logger.LogInformation(1, "EventID ありで出力します。");
+		}
+
+		public void OnGet()
+		{
+			_logger.LogInformation("ページを表示するタイミングでログを出力します。");
+		}
+	}
+}
---
+++ LogNLog/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using NLog.Web;
+
+namespace LogNLog
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+      try
+      {
+        CreateHostBuilder(args).Build().Run();
+      }
+      catch (Exception exception)
+      {
+        // NLog：セットアップエラーをキャッチ
+        logger.Error(exception, "例外のためにプログラムを停止しました。");
+        throw;
+      }
+      finally
+      {
+        // アプリケーションを終了する前に、内部タイマー/スレッドをフラッシュして停止するようにしてください
+        // (Linux でのセグメンテーション違反を回避してください）
+        NLog.LogManager.Shutdown();
+      }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        })
+        .ConfigureLogging(logging =>
+        {
+          logging.ClearProviders();                 // NLog 以外で設定された Provider の無効化.
+          logging.SetMinimumLevel(LogLevel.Trace);  // 最小ログレベルの設定
+        })
+        .UseNLog();  // NLog：依存性注入のための NLog のセットアップ
+  }
+}
